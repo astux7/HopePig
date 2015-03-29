80490352d44162facdbdf4c33930e9515fa79e68
astux7 <Asta.bevainyte@gmail.com>
2015-03-29 20:06:50 +0100

    android v 21 / 18

--- Rakefile
+++ Rakefile
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 $:.unshift("/Library/RubyMotion/lib")
 require 'motion/project/template/android'
-
+#/Users/astux7/android-rubymotion/sdk/tools/android
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'HopePig'
-  app.api_version = "18"
-  app.icon = 'icon'
+  app.api_version = "21"
+  app.icon = 'iclauncherxhdpi'
 end
--- app/main_activity.rb
+++ app/main_activity.rb
@@ -1,17 +1,9 @@
 class MainActivity < Android::App::Activity
-  attr_reader :webview, :handler
+  attr_reader :webview
 
   def onCreate(savedInstanceState)
     super
-    @handler = Android::Os::Handler.new
-
     @webview = Android::Webkit::WebView.new(self)
-    settings = @webview.settings
-    settings.savePassword = false
-    settings.saveFormData = false
-    settings.javaScriptEnabled = true
-    settings.supportZoom = false
-
     @webview.loadUrl("file:///android_asset/hopepig.html")
 
     self.contentView = @webview
